1e3fa0430de3c5109a4fdfbff0bdd4cc28d8d292
Kevin Huang <thekevinhuang@gmail.com>
2019-02-18 10:08:37 -0500

    added general reducer structure as placeholder

---
+++ src/reducers/itineraryReducer.js
@@ -0,0 +1,14 @@
+const initialState= {
+    name: '',
+    description: '',
+    days: []
+}
+
+function itineraryReducer (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default itineraryReducer
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,8 @@
 import {combineReducers} from "redux"
+import itineraryReducer from './itineraryReducer'
 
 const rootReducer = combineReducers({
-
+    itineraries: itineraryReducer
 })
 
 export default rootReducer
