b14f08d01775d79f2052a2f2fef949cee77daf11
Kevin Huang <thekevinhuang@gmail.com>
2019-02-22 00:20:08 -0500

    adding form to point new

--- src/components/points/PointNew.js
+++ src/components/points/PointNew.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react'
 
+const initialState = {
+    name : '',
+    description: ''
+}
+
 export default class PointNew extends Component {
+    constructor() {
+        super()
+        this.state=initialState
+    }
+
+    //create handleChange and handleSubmit
+
     render() {
         return (
             <div>
-                
+                <form onSubmit={this.handleSubmit}>
+                    <label>Point Name</label>
+                    <input type="text" name="name" onChange={this.handleChange} value={this.state.name}/><br/>
+
+                    <label>Point Description</label>
+                    <input type="text" name="description" onChange={this.handleChange} value={this.state.description}/><br/>
+
+                    <input type="submit" value="Create a new Point!"/>
+                </form>
             </div>
         )
     }
